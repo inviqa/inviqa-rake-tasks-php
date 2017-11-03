0715339538dfd1dbc7867ba8b015e910248310ed
Kieren Evans <kevans@inviqa.com>
2017-11-03 17:17:03 +0000

    Allow specifying arbitrary arguments

--- lib/rake-tasks-php/rake_task.rb
+++ lib/rake-tasks-php/rake_task.rb
@@ -1,9 +1,25 @@
 namespace :php do
+  def parse_php_args(args)
+    args = args[2..-1] if args.length > 2
+    args
+  end
+
   task :behat do
-    services_from_args(services: %w[web]).exec('build', 'php bin/behat')
+    args = parse_php_args(ARGV)
+    services_from_args(services: %w[web]).exec('build', "php bin/behat #{args.join(' ')}")
+    exit $?.to_i
+  end
+
+  task :composer do
+    args = parse_php_args(ARGV)
+    services_from_args(services: %w[web]).exec('build', "composer #{args.join(' ')}")
+    exit $?.to_i
   end
 
   task :phpspec do
-    services_from_args(services: %w[web]).exec('build', 'php bin/phpspec run')
+    args = parse_php_args(ARGV)
+    args = ['run'] if args.empty?
+    services_from_args(services: %w[web]).exec('build', "php bin/phpspec #{args.join(' ')}")
+    exit $?.to_i
   end
 end
